f3f376024541ffceeb0e751fe9059a2f3839017d
Aliaksandr Karaliou <aliaksandr.karaliou@itechart-group.com>
2022-11-25 00:24:24 +0100

    Added login functionality

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { PropTypes } from "prop-types";
 
 import "./Login.css";
 
+async function loginUser(creds) {
+
+    return fetch('http://localhost:4001/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(creds)
+    })
+    .then(data => data.json())
+}
+
 const Login = ({setToken}) => {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            email: username,
+            password
+        });
+        console.log(token)
+        setToken(token);
+    }
+
     return(
         <div className="login-wrapper">
             <h1>Please Log In</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type="text"/>
+                    <input type="text" onChange={e => setUserName(e.target.value)} />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input type="password" onChange={e => setPassword(e.target.value)} />
                 </label>
                 <div>
                     <button type="submit">Submit</button>
